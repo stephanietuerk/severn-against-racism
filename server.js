9f6a48c6393e8b5a18f6cff16665c0489af419d9
Stephanie Tuerk <STuerk@mathematica-mpr.com>
2020-07-04 15:22:17 -0400

    made changes to package.json and server.js for heroku

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
+app.use(express.static('./dist/severn-anti-racism'));
 
 const forceSSL = function() {
-return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-    return res.redirect(
-        ['https://', req.get('Host'), req.url].join('')
-    );
+    return function (req, res, next) {
+        if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(
+            ['https://', req.get('Host'), req.url].join('')
+        );
     }
-    next();
-}
+        next();
+    }
 }
 
 app.use(forceSSL());
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile('index.html', {root: 'dist/sever-anti-racism/'});
 });
+console.log('starting');
+app.listen(process.env.PORT || 8080);
