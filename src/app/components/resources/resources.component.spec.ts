03b9c09aa430a5021b44d88d3667e019d248a08e
Stephanie Tuerk <STuerk@mathematica-mpr.com>
2020-07-06 01:15:39 -0400

    changed media to resources and moved doc table there

--- src/app/components/resources/resources.component.spec.ts
+++ src/app/components/resources/resources.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { RelatedComponent } from './related.component';
+import { ResourcesComponent } from './resources.component';
 
-describe('RelatedComponent', () => {
-  let component: RelatedComponent;
-  let fixture: ComponentFixture<RelatedComponent>;
+describe('ResourcesComponent', () => {
+  let component: ResourcesComponent;
+  let fixture: ComponentFixture<ResourcesComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RelatedComponent ]
+      declarations: [ ResourcesComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(RelatedComponent);
+    fixture = TestBed.createComponent(ResourcesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
